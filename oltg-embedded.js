adf77d78df76fb35f884daefdb2e67a8efe03b89
Noah Hoffman <noah.hoffman@gmail.com>
2019-02-11 14:55:11 -0800

    implements page with list of tests

--- oltg-embedded.js
+++ oltg-embedded.js
@@ -1,3 +1,7 @@
+// var oltgApi = "https://oltgtest.labmed.uw.edu/public/api/v2/";
+// var oltgApi = "https://testguide.labmed.uw.edu/public/api/v2/";
+var oltgApi = "http://localhost:5000/api/v2/";
+
 function getUrlParameter(name) {
   name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
   var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
@@ -5,15 +9,27 @@ function getUrlParameter(name) {
   return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 };
 
-$( document ).ready(function() {
-  let baseurl = "https://testguide.labmed.uw.edu/public/api/v2/mnemonic/";
-  // let baseurl = "http://localhost:5000/api/v2/mnemonic/";
-  let mnemonic = getUrlParameter('mnemonic');
-  let url = baseurl + mnemonic;
-  $.ajaxSetup({ cache: false });
-  $.getJSON(url, function(data) {
-    var template = $("#oltg-view-template").html();
-    var text = Mustache.render(template, data);
-    $("#oltg-embed").html(text);
+function embedOLTGView() {
+  $( document ).ready(function() {
+    let mnemonic = getUrlParameter('mnemonic');
+    let url = oltgApi + "mnemonic/" + mnemonic;
+    $.ajaxSetup({ cache: false });
+    $.getJSON(url, function(data) {
+      var template = $("#oltg-view-template").html();
+      var text = Mustache.render(template, data);
+      $("#oltg-embed").html(text);
+    });
   });
-});
+};
+
+function embedOLTGList(mnemonics) {
+  $( document ).ready(function() {
+    let url = oltgApi + "search/?mnemonic=" + mnemonics;
+    $.ajaxSetup({ cache: false });
+    $.getJSON(url, function(data) {
+      var template = $("#oltg-list-template").html();
+      var text = Mustache.render(template, data);
+      $("#oltg-embed").html(text);
+    });
+  });
+};
