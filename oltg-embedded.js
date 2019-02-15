0af93ce76373d93b49726256168c7af03cce8620
Noah Hoffman <noah.hoffman@gmail.com>
2019-02-15 06:52:56 -0800

    wip, working on url params

--- oltg-embedded.js
+++ oltg-embedded.js
@@ -1,6 +1,6 @@
 // var oltgApi = "https://testguide.labmed.uw.edu/public/api/v2/";
-var oltgApi = "https://oltgtest.labmed.uw.edu/public/api/v2/";
-// var oltgApi = "http://localhost:5000/api/v2/";
+// var oltgApi = "https://oltgtest.labmed.uw.edu/public/api/v2/";
+var oltgApi = "http://localhost:5000/api/v2/";
 
 function getUrlParameter(name) {
   name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
@@ -12,12 +12,15 @@ function getUrlParameter(name) {
 function embedOLTGView() {
   $( document ).ready(function() {
     let mnemonic = getUrlParameter('mnemonic');
-    let url = oltgApi + "mnemonic/" + mnemonic;
+    console.log(mnemonic);
+    console.log(location.pathname);
+    let url = oltgApi + "mnemonic/" + mnemonic + '?prefix=' + location.pathname;
+    console.log(url);
     $.ajaxSetup({ cache: false });
     $.getJSON(url, function(data) {
       var template = $("#oltg-view-template").html();
       var text = Mustache.render(template, data);
-      $("#oltg-embed").html(text);
+      $("#oltg-embed-view").html(text);
     });
   });
 };
@@ -25,13 +28,14 @@ function embedOLTGView() {
 function embedOLTGList(mnemonics) {
   $( document ).ready(function() {
     let url = oltgApi + "search?order_by=name&mnemonic=" + mnemonics;
+    console.log(url);
     $.ajaxSetup({ cache: false });
     $.getJSON(url, function(data) {
       // inject prefix for links, assuming that view.html is in the same path
       data.prefix = location.pathname;
       var template = $("#oltg-list-template").html();
       var text = Mustache.render(template, data);
-      $("#oltg-embed").html(text);
+      $("#oltg-embed-list").html(text);
     });
   });
 };
