7567cb794db4559af953b3e649394e11997268f5
Noah Hoffman <noah.hoffman@gmail.com>
2019-02-18 09:25:11 -0800

    update to use 'code' argument to view api

--- oltg-embedded.js
+++ oltg-embedded.js
@@ -1,6 +1,6 @@
 // var oltgApi = "https://testguide.labmed.uw.edu/public/api/v2/";
-// var oltgApi = "https://oltgtest.labmed.uw.edu/public/api/v2/";
-var oltgApi = "http://localhost:5000/api/v2/";
+var oltgApi = "https://oltgtest.labmed.uw.edu/public/api/v2/";
+// var oltgApi = "http://localhost:5000/api/v2/";
 
 function getUrlParameter(name) {
   name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
@@ -11,10 +11,9 @@ function getUrlParameter(name) {
 
 function embedOLTGView() {
   $( document ).ready(function() {
-    let mnemonic = getUrlParameter('mnemonic');
-    console.log(mnemonic);
-    console.log(location.pathname);
-    let url = oltgApi + "mnemonic/" + mnemonic + '?prefix=' + location.pathname;
+    let code = getUrlParameter('code');
+    console.log(code);
+    let url = oltgApi + "view" + '?code=' + code;
     console.log(url);
     $.ajaxSetup({ cache: false });
     $.getJSON(url, function(data) {
